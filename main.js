fed86537a3b1a0411c0f483ce4e76acaa28bc9ec
simonfotnt <simonpiar98@gmail.com>
2023-10-03 19:01:22 +0600

    add new questions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const questions = [
     {
-        question:" which is largest naimal in the world?",
+        question:" which is largest animal in the world?",
         answer:[
             {text: "shark", correct: false},
             {text: "Blue whale", correct: true},
@@ -34,6 +34,76 @@ const questions = [
             {text: "America", correct: false},
             {text: "Bangladesh", correct: false},
             {text: "Fish", correct: false},
+        ]
+    },
+    {
+        question:" Our country is spriiual country, theirs .... religious?",
+        answer:[
+            {text: "is", correct: true},
+            {text: "are", correct: false},
+            {text: "also", correct: false},
+            {text: "have", correct: false},
+        ]
+    },
+    {
+        question:" Our sir teaches Mathematics .... English.",
+        answer:[
+            {text: "both", correct: false},
+            {text: "across", correct: false},
+            {text: "beside", correct: false},
+            {text: "besides", correct: true},
+        ]
+    },
+    {
+        question:" Please, come .... the bathroom?",
+        answer:[
+            {text: "out of", correct: true},
+            {text: "over", correct: false},
+            {text: "on", correct: false},
+            {text: "in", correct: false},
+        ]
+    },
+    {
+        question:" I .... with guest when you called me.",
+        answer:[
+            {text: "discussed", correct: false},
+            {text: "am discussing", correct: false},
+            {text: "was discussing", correct: true},
+            {text: "was being discussed", correct: false},
+        ]
+    },
+    {
+        question:" Gifts .... among the students now.",
+        answer:[
+            {text: "are distributing", correct: false},
+            {text: "were distributing", correct: false},
+            {text: "are being distributted", correct: false},
+            {text: "are being distributed", correct: true},
+        ]
+    },
+    {
+        question:" Humayun was born in the year .....",
+        answer:[
+            {text: "1508", correct: true},
+            {text: "1608", correct: false},
+            {text: "1708", correct: false},
+            {text: "1808", correct: false},
+        ]
+    },{
+        question:" Who invented the 3D printer?",
+        answer:[
+            {text: "Elias howe", correct: false},
+            {text: "Chuck Hull", correct: true},
+            {text: "Nick Holonyak", correct: false},
+            {text: "Christaian Huygens", correct: false},
+        ]
+    },{
+        question:" The Grand canyon located in which country?",
+        answer:[
+            {text: "Ghana", correct: false},
+            {text: "The US", correct: true},
+            {text: "Canada", correct: false},
+            {text: "Bolivia", correct: false},
         ]
     }
 ];
